81cc109c6a98bfd79ffad1a77b893198f2b245c5
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-02-03 22:33:29 +0000

    Changes

--- VidToAscii/makeFrames.js
+++ VidToAscii/makeFrames.js
@@ -5,10 +5,23 @@ const inputVideo = 'badapple.mp4';
 const frameDirectory = 'frames/';
 const fps = 10;
 
+
+// Record the current time
+const startTime = new Date().getTime();
+console.log("Making frames...")
 exec(`ffmpeg -i ${inputVideo} -vf fps=${fps} ${frameDirectory}frame%d.jpg`, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
   }
+  console.log("Frames made!");
+  console.log(`Took ${(new Date().getTime() - startTime) / 1000} seconds`);
+  // Get the last file from frames/ 
+  exec(`ls -t ${frameDirectory} | head -1`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Last frame: ${stdout}`);
+  })
 });
-
